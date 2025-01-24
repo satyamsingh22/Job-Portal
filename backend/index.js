5717a941e0526ad6b99eaf7368f91ddc89a60a42
Satyam Singh <116868686+satyamsingh22@users.noreply.github.com>
2025-01-24 17:49:56 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
-import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
+import express from "express";
+import applicationRoute from "./routes/application.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
-import applicationRoute from "./routes/application.route.js";
+import userRoute from "./routes/user.route.js";
+import connectDB from "./utils/db.js";
 
 dotenv.config({});
 
