af2b3ba5c1d47ce656fe92855c93469649d6751f
Satyam Singh <116868686+satyamsingh22@users.noreply.github.com>
2025-01-25 13:46:38 +0530

    url coorection

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
 export const USER_API_END_POINT="https://job-portal-hchg.onrender.com/api/v1/user";
 export const JOB_API_END_POINT="https://job-portal-hchg.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://job-portal-hchg.onrender.com/application";
+export const APPLICATION_API_END_POINT="https://job-portal-hchg.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT="https://job-portal-hchg.onrender.com/api/v1/company";
